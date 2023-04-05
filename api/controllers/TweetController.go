39152d72832f2da155eecb6dc80c380021790156
Breno M. Silva <sbrenomartins@gmail.com>
2023-04-05 20:16:15 -0300

    first commit

---
+++ api/controllers/TweetController.go
@@ -0,0 +1,62 @@
+package controllers
+
+import (
+	"net/http"
+	entities "primeira-api-go/api/entities"
+
+	"github.com/gin-gonic/gin"
+)
+
+type tweetController struct {
+	tweets []entities.Tweet
+}
+
+func NewTweetController() *tweetController {
+	return &tweetController{}
+}
+
+func (t *tweetController) FindAll(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, t.tweets)
+}
+
+func (t *tweetController) FindById(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	for idx, tweet := range t.tweets {
+		if tweet.ID == id {
+			ctx.JSON(http.StatusOK, t.tweets[idx])
+			return
+		}
+	}
+
+	ctx.JSON(http.StatusNotFound, gin.H{
+		"error": "Tweet not found",
+	})
+}
+
+func (t *tweetController) Create(ctx *gin.Context) {
+	tweet := entities.NewTweet()
+
+	if err := ctx.BindJSON(&tweet); err != nil {
+		return
+	}
+
+	t.tweets = append(t.tweets, *tweet)
+
+	ctx.JSON(http.StatusOK, tweet)
+}
+
+func (t *tweetController) Delete(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	for idx, tweet := range t.tweets {
+		if tweet.ID == id {
+			t.tweets = append(t.tweets[0:idx], t.tweets[idx+1:]...)
+			return
+		}
+	}
+
+	ctx.JSON(http.StatusNotFound, gin.H{
+		"error": "Tweet not found",
+	})
+}
---
+++ api/routes/main.go
@@ -0,0 +1,21 @@
+package routes
+
+import (
+	controllers "primeira-api-go/api/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AppRoutes(router *gin.Engine) *gin.RouterGroup {
+	tweetController := controllers.NewTweetController()
+
+	v1 := router.Group("/v1")
+	{
+		v1.GET("/tweets", tweetController.FindAll)
+		v1.GET("/tweet/:id", tweetController.FindById)
+		v1.POST("/tweet", tweetController.Create)
+		v1.DELETE("/tweet/:id", tweetController.Delete)
+	}
+
+	return v1
+}
